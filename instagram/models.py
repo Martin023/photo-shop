dca292d8601e3f49c03735cf8435008cdf1b1ec0
martin023 <mainamartin023@gmail.com>
2022-06-09 22:36:57 +0300

    This commit will add image model

--- instagram/models.py
+++ instagram/models.py
@@ -32,44 +32,17 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
     
-   
+    @classmethod
+    def filter_profile_by_id(cls, id):
+        profile = Profile.objects.filter(user__id = id).first()
+        return profile
 
     @classmethod
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
 
 
-class Image(models.Model):
-    image = CloudinaryField('image')
-    name = models.CharField(max_length=250, blank=True)
-    caption = models.CharField(max_length=250, blank=True)
-    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
-    created = models.DateTimeField(auto_now_add=True, null=True)
-
-    class Meta:
-        ordering = ["-pk"]
-
-    def save_post(self):
-        self.save()
-
-    def delete_post(self):
-        self.delete()
-
-    def total_likes(self):
-        return self.likes.count()
-    
-    @classmethod
-    def get_profile_posts(cls,profile):
-        posts = Image.objects.filter(profile__pk= profile)
-        return posts
-    @classmethod
-    def update_post_caption(cls,id,caption):
-        update =cls.objects.filter(id=id).update(caption=caption)
-        return update
-
-    def __str__(self):
-        return f'{self.user.name} Post'
+er.name} Post'
 
 
 class Comments(models.Model):
