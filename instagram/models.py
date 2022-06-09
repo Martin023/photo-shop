e104f6764c20aa50afd956a8497071195c2eb2f8
martin023 <mainamartin023@gmail.com>
2022-06-09 22:43:09 +0300

    This commit will add save and delete functions for comments

--- instagram/models.py
+++ instagram/models.py
@@ -75,6 +75,23 @@ class Image(models.Model):
         return f'{self.user.name} Post'
 
 
+class Comments(models.Model):
+    comment = models.CharField(max_length=100)
+    post=models.ForeignKey(Image,related_name='comments',on_delete=models.CASCADE ,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+   
+    
+    @classmethod
+    def filter_comments_by_post_id(cls, id):
+        comments = Comments.objects.filter(post__id=id)
+        return comments
+    
+
+    class Meta:
+        ordering = ["-pk"]
+
 
 class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
